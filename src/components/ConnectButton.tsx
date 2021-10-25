366c562c92b86392adc0d6c3b5c5fd9d93eae82f
Gabriel Pickard <wergomat@gmail.com>
2021-10-25 13:39:59 -0500

    address instead of ethers in connect button

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,20 +1,21 @@
 import { Box, Text, Button } from "@chakra-ui/react";
-import { formatEther } from "@ethersproject/units";
-import { useEthers, useEtherBalance } from "@usedapp/core";
-import React from "react";
+import { useEthers, shortenAddress } from "@usedapp/core";
+import React, { useContext } from "react";
+import { UserAddressContext } from "./UserAddressContext";
 
 export default function ConnectButton() {
-  const { activateBrowserWallet, account } = useEthers();
-  const etherBalance = useEtherBalance(account);
+  const { activateBrowserWallet } = useEthers();
 
   function handleConnectWallet() {
     activateBrowserWallet();
   }
 
+  const account = useContext(UserAddressContext);
+
   return account ? (
     <Box sx={{ float: "right" }}>
       <Text>
-        {etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3)} ETH
+        {shortenAddress(account)}
       </Text>
     </Box>
   ) : (

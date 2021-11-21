e2354d41962ec0f2ba973899f187efe44362b47d
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2021-11-21 14:39:10 -0300

    #57 Shorten transaction hash output in toast
    https://github.com/MoreMoney-Finance/frontend/issues/57

--- src/components/NotificationsComponent.tsx
+++ src/components/NotificationsComponent.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 import { AlertStatus, useToast } from '@chakra-ui/react';
-import { getExplorerTransactionLink, useNotifications } from '@usedapp/core';
+import {
+  ChainId,
+  getExplorerTransactionLink,
+  useEthers,
+  useNotifications,
+} from '@usedapp/core';
 
 export const NotificationsComponent: React.FC = () => {
   const { notifications } = useNotifications();
   const toast = useToast();
+  const { chainId } = useEthers();
+  const _chainId = chainId === ChainId.Hardhat ? ChainId.Avalanche : chainId;
 
   function showToast(title: string, status: AlertStatus, notification: any) {
-    const explorerLink = getExplorerTransactionLink(
-      notification.transaction.hash,
-      notification.transaction.chainId
-    );
+    const hash = notification.transaction.hash;
+    const explorerLink = getExplorerTransactionLink(hash, _chainId!);
     toast({
       id: notification.id,
       title: title,
@@ -21,7 +26,7 @@ export const NotificationsComponent: React.FC = () => {
           rel="noreferrer"
           style={{ textDecoration: 'underline' }}
         >
-          {notification.transaction.hash}
+          {`${hash.slice(0, 6)}...${hash.slice(hash.length - 4, hash.length)}`}
         </a>
       ),
       status: status,

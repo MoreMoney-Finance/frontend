e3c20b42d7c6c51832e4bf08d7e45e620c935e72
Gabriel Pickard <wergomat@gmail.com>
2021-12-28 18:49:57 -0600

    Update TokenDescription.tsx

--- src/components/TokenDescription.tsx
+++ src/components/TokenDescription.tsx
@@ -21,7 +21,7 @@ export function TokenDescription({
             <Avatar src={iconUrl} key={i + 1} />
           ))}
         </AvatarGroup>
-        <Text size={textSize}>{token.symbol}</Text>
+        <Text size={textSize}>{token.ticker}</Text>
       </Flex>
     </HStack>
   ) : (

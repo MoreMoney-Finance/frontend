52c90f9df21c038ca478059053ef4b2fd88810e6
Gabriel Pickard <wergomat@gmail.com>
2022-04-05 11:52:20 -0500

    add yy strategy for lpts

--- src/chain-interaction/contracts.ts
+++ src/chain-interaction/contracts.ts
@@ -333,6 +333,8 @@ export function useIsolatedStrategyMetadata(): StrategyMetadata {
     ['0xd586e7f844cea2f87f50152665bcbc2c279d8d70']: addresses.YieldYakStrategy,
     ['0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5']: addresses.YieldYakStrategy,
     ['0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664']: addresses.YieldYakStrategy,
+    ['0xa389f9430876455c36478deea9769b7ca4e3ddb1']: addresses.YieldYakStrategy,
+    ['0xed8cbd9f0ce3c6986b22002f03c6475ceb7a6256']: addresses.YieldYakStrategy,
     ['0x454E67025631C065d3cFAD6d71E6892f74487a15']:
       addresses.TraderJoeMasterChefStrategy,
     ['0x2148D1B21Faa7eb251789a51B404fc063cA6AAd6']:
@@ -345,13 +347,16 @@ export function useIsolatedStrategyMetadata(): StrategyMetadata {
     '0x57319d41f71e81f3c65f2a47ca4e001ebafd4f33',
     '0xa389f9430876455c36478deea9769b7ca4e3ddb1',
     '0xed8cbd9f0ce3c6986b22002f03c6475ceb7a6256',
-    '0xd5a37dc5c9a396a03dd1136fc76a1a02b1c88ffa',
   ].map(getAddress);
 
   const tokens = Object.keys(token2Strat);
   tokens.push('0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7');
   const strats = Object.values(token2Strat);
   strats.push(addresses.LiquidYieldStrategy);
+
+  tokens.push('0x454E67025631C065d3cFAD6d71E6892f74487a15');
+  strats.push(addresses.YieldYakStrategy);
+
   const globalMoneyAvailable = globalDebtCeiling.sub(totalSupply);
 
   tokens.push('0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd');

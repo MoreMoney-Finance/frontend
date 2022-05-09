d335f97b4c16b1344392e383c53127d2bfefd5f2
Gabriel Pickard <wergomat@gmail.com>
2022-05-09 11:52:18 -0500

    Update index.tsx

--- src/pages/LiquidatablePositions/index.tsx
+++ src/pages/LiquidatablePositions/index.tsx
@@ -81,7 +81,7 @@ export default function LiquidatablePositions() {
 
   return (
     <>
-      {liquidatablePositions.length > 0 ? (
+      {liquidatablePositions.length > 0 && false ? (
         <VStack mt="40px">
           <h1>Liquidatable positions</h1>
           <LiquidatablePositionsTable

5a19dc6d636773a412c1451a3e3b146573a71454
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-05-16 10:51:10 -0300

    Update contracts.ts

--- src/chain-interaction/contracts.ts
+++ src/chain-interaction/contracts.ts
@@ -88,6 +88,8 @@ export type DeploymentAddresses = {
   LiquidYieldStrategy: string;
   MultiTraderJoeMasterChef3Strategy: string;
   InterestRateController: string;
+  VeMore: string;
+  xMoney: string;
 };
 
 export function useAddresses() {
@@ -176,6 +178,8 @@ export type ParsedStratMetaRow = {
   harvestBalance2Tally: CurrencyValue;
   yieldType: YieldType;
   balance: number;
+  selfRepayingAPY: number;
+  compoundingAPY: number;
 };
 
 function parseStratMeta(
@@ -222,6 +226,47 @@ function parseStratMeta(
           strategyAddress in additionalYield[token.address]
         ? additionalYield[token.address][strategyAddress]
         : 0;
+    const selfRepayingAPY =
+      row.yieldType === 0
+        ? strategyAddress === addresses[chainId].LiquidYieldStrategy
+          ? token.address === '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7'
+            ? ((yieldMonitor['0x4b946c91C2B1a7d7C40FB3C130CdfBaf8389094d']
+                .totalApy -
+                parseFloat(
+                  yieldMonitor[
+                    '0x4b946c91C2B1a7d7C40FB3C130CdfBaf8389094d'
+                  ].apy.toString()
+                )) *
+                0.65 *
+                0.8) /
+              0.5
+            : ((yieldMonitor['0x4b946c91C2B1a7d7C40FB3C130CdfBaf8389094d']
+                .totalApy -
+                parseFloat(
+                  yieldMonitor[
+                    '0x4b946c91C2B1a7d7C40FB3C130CdfBaf8389094d'
+                  ].apy.toString()
+                )) *
+                0.3 *
+                0.8) /
+                0.5 +
+              8
+          : token.address in yieldMonitor
+          ? yieldMonitor[token.address].totalApy -
+            parseFloat(yieldMonitor[token.address].apy.toString())
+          : token.address in additionalYield &&
+            strategyAddress in additionalYield[token.address]
+          ? additionalYield[token.address][strategyAddress]
+          : 0
+        : 0;
+
+    const compoundingAPY =
+      row.yieldType === 0 && token.address in yieldMonitor
+        ? parseFloat(yieldMonitor[token.address].apy.toString())
+        : strategyAddress === addresses[chainId].YieldYakStrategy ||
+          strategyAddress === addresses[chainId].YieldYakAVAXStrategy
+        ? APY
+        : 0;
 
     let syntheticDebtCeil = globalMoneyAvailable.add(row.totalDebt);
 
@@ -253,6 +298,8 @@ function parseStratMeta(
         row.yieldType
       ],
       balance: parseFloatCurrencyValue(balance),
+      selfRepayingAPY: selfRepayingAPY,
+      compoundingAPY: compoundingAPY,
     };
   }
 }
@@ -579,6 +626,17 @@ export function useIsolatedPositionMetadata(
     : legacyResults;
 }
 
+export function xMoneyTotalSupply(defaultResult: any) {
+  const address = useAddresses().xMore;
+  const abi = new Interface(xMore.abi);
+  return (useContractCall({
+    abi,
+    address,
+    method: 'totalSupply',
+    args: [],
+  }) ?? [defaultResult])[0];
+}
+
 export function xMoreTotalSupply(
   method: string,
   args: any[],
@@ -601,6 +659,35 @@ export function useGlobalDebtCeiling(
 ) {
   const address = useAddresses().Stablecoin;
   const abi = new Interface(Stablecoin.abi);
+  return (useContractCall({
+    abi,
+    address,
+    method,
+    args,
+  }) ?? [defaultResult])[0];
+}
+
+export function useBalanceOfToken(
+  address: string,
+  args: any[],
+  defaultResult: any
+) {
+  const abi = new Interface(ERC20.abi);
+  return (useContractCall({
+    abi,
+    address,
+    method: 'balanceOf',
+    args,
+  }) ?? [defaultResult])[0];
+}
+
+export function useTotalSupplyToken(
+  address: string,
+  method: string,
+  args: any[],
+  defaultResult: any
+) {
+  const abi = new Interface(ERC20.abi);
   return (useContractCall({
     abi,
     address,

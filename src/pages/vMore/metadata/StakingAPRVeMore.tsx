e45c6e5794c34f51a3648dc3c11e7bcba948ae4b
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-05-30 17:46:42 -0300

    #333 stakedMore and claimable information
    Fixes: https://github.com/MoreMoney-Finance/frontend/issues/333

--- src/pages/vMore/metadata/StakingAPRVeMore.tsx
+++ src/pages/vMore/metadata/StakingAPRVeMore.tsx
@@ -7,12 +7,20 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { formatEther } from '@ethersproject/units';
+import { CurrencyValue, useEthers } from '@usedapp/core';
+import { BigNumber } from 'ethers';
 import * as React from 'react';
 import lines from '../../../assets/img/lines.svg';
 import {
   useAddresses,
   useTotalSupplyToken,
 } from '../../../chain-interaction/contracts';
+import { getTokenFromAddress } from '../../../chain-interaction/tokens';
+import {
+  useGetStakedMoreVeMoreToken,
+  useClaimableVeMoreToken,
+} from '../../../chain-interaction/transactions';
+import { UserAddressContext } from '../../../contexts/UserAddressContext';
 import { formatNumber } from '../../../utils';
 
 export function StakingAPRVeMoreToken(props: React.PropsWithChildren<unknown>) {
@@ -20,6 +28,26 @@ export function StakingAPRVeMoreToken(props: React.PropsWithChildren<unknown>) {
   const totalSupplyStakedVeMore = formatEther(
     useTotalSupplyToken(addresses.VeMoreToken, 'totalSupply', [], 0)
   );
+  const account = React.useContext(UserAddressContext);
+  const { chainId } = useEthers();
+  const veMoreTokenToken = getTokenFromAddress(
+    chainId,
+    useAddresses().VeMoreToken
+  );
+  const moreToken = getTokenFromAddress(chainId, useAddresses().MoreToken);
+
+  const stakedMore = useGetStakedMoreVeMoreToken(account!);
+  const claimableVeMoreToken = useClaimableVeMoreToken(account!);
+
+  const stakedMoreBalance = new CurrencyValue(
+    moreToken,
+    BigNumber.from(stakedMore)
+  );
+  const claimableVeMoreTokenBalance = new CurrencyValue(
+    veMoreTokenToken,
+    BigNumber.from(claimableVeMoreToken)
+  );
+
   return (
     <GridItem rowSpan={[12, 12, 1]} colSpan={[12, 12, 1]}>
       <Container variant={'token'} padding={'30px'}>
@@ -39,18 +67,26 @@ export function StakingAPRVeMoreToken(props: React.PropsWithChildren<unknown>) {
           h={'full'}
           padding={'30px 80px 40px 40px'}
         >
-          {/* <Flex w={'full'}>
+          <Flex w={'full'} marginTop={'30px'}>
             <Text variant="h200" color={'whiteAlpha.400'}>
-              Approximate APR
+              Staked MORE
+            </Text>
+            <Spacer />
+            <Text variant={'bodyLarge'}>{stakedMoreBalance.format()}</Text>
+          </Flex>
+          <Flex w={'full'} marginTop={'30px'}>
+            <Text variant="h200" color={'whiteAlpha.400'}>
+              Claimable
             </Text>
             <Spacer />
             <Text variant={'bodyLarge'}>
-              {xMoreData?.cachedAPR?.toFixed(2)}%
+              {' '}
+              {claimableVeMoreTokenBalance.format()}
             </Text>
-          </Flex> */}
+          </Flex>
           <Flex w={'full'} marginTop={'30px'}>
             <Text variant="h200" color={'whiteAlpha.400'}>
-              Total Supply
+              Total veMore Supply
             </Text>
             <Spacer />
             <Text variant={'bodyLarge'}>

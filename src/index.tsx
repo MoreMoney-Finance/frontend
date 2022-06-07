cc2c371694c47bdb44653a13535e4832f168a89c
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-06-07 15:16:28 -0300

    #341 Positions Page home page
    https://github.com/MoreMoney-Finance/frontend/issues/341

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,9 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { App } from './App';
-import { LiquidatablePositionsCtxProvider } from './contexts/LiquidatablePositionsContext';
-import AdminPage from './pages/Admin';
-import Analytics from './pages/Analytics';
-import Dashboard from './pages/Dashboard';
 import { ErrorBoundary } from './pages/ErrorBoundary/ErrorBoundary';
-import FarmPage from './pages/Farm';
-import LiquidatablePositions from './pages/LiquidatablePositions';
-import LiquidationProtectedLoans from './pages/Loans';
 import PositionsPage from './pages/Positions';
 import TokenPage from './pages/TokenPage';
-import XMorePage from './pages/XMore';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
@@ -36,7 +28,7 @@ ReactDOM.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
-            <Route index element={<Dashboard />} />
+            <Route index element={<PositionsPage />} />
             <Route
               path="/token/:tokenAddress"
               element={
@@ -45,21 +37,7 @@ ReactDOM.render(
                 </ErrorBoundary>
               }
             />
-            <Route path="/farm" element={<FarmPage />} />
             <Route path="/positions" element={<PositionsPage />} />
-            <Route
-              path="/liquidatable-positions"
-              element={
-                <LiquidatablePositionsCtxProvider>
-                  <LiquidatablePositions />
-                </LiquidatablePositionsCtxProvider>
-              }
-            />
-            <Route path="/loans" element={<LiquidationProtectedLoans />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/xmore" element={<XMorePage />} />
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="/admin/:positions" element={<AdminPage />} />
           </Route>
         </Routes>
       </BrowserRouter>

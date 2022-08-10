435db98b0e2e763bb3d96d84725c8ed7c190c71b
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-08-10 10:06:11 -0300

    Create strategies-naming.ts

---
+++ src/constants/strategies-naming.ts
@@ -0,0 +1,9 @@
+export const strategyNames = {
+  '0xf9cD4Db17a3FB8bc9ec0CbB34780C91cE13ce767': 'PlatypusStrategy',
+  '0x8889Da43CeE581068C695A2c256Ba2D514608F4A': 'BenqiStrategyV3',
+  '0xd0F41b1C9338eB9d374c83cC76b684ba3BB71557': 'VectorStrategyForSAV2',
+  '0xc8cEeA18c2E168C6e767422c8d144c55545D23e9': 'BenqiStrategyV3',
+  '0xaAc0F2d0630d1D09ab2B5A400412a4840B866d95': 'AaveV3StrategyAvaxV1',
+  '0x8B414448de8B609e96bd63Dcf2A8aDbd5ddf7fdd': 'BenqiStrategyAvaxV1',
+  '0x9f637540149f922145c06e1aa3f38dcDc32Aff5C': 'GmxStrategyForGLP',
+};

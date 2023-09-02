2f577efdae5b36b20f31a96b2a451d6fa75a569d
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2023-09-02 19:18:14 -0300

    Update NFTSection.tsx

--- src/components/nft/NFTSection.tsx
+++ src/components/nft/NFTSection.tsx
@@ -1,4 +1,5 @@
 import { BigNumber } from 'ethers';
+import { getAddress } from 'ethers/lib/utils';
 import * as React from 'react';
 import { useCurrentEpoch } from '../../chain-interaction/contracts';
 import { ExternalMetadataContext } from '../../contexts/ExternalMetadataContext';
@@ -19,17 +20,7 @@ const NFTSection: React.FC<Props> = ({}) => {
   const isEligible =
     nftSnapshot &&
     account &&
-    nftSnapshot.eligible[currentEpoch.toString()][account];
-  console.log({
-    currentEpoch,
-    account,
-    nftSnapshot,
-    noNFTFound,
-    isEligible:
-      nftSnapshot &&
-      account &&
-      nftSnapshot.eligible[currentEpoch.toString()][account],
-  });
+    nftSnapshot.eligible[currentEpoch.toString()][getAddress(account)];
 
   return <>{isEligible && noNFTFound && <ClaimNFT />}</>;
 };

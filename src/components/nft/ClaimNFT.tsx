8d9010426bf2509534e72a5a8a3546f38d0ccf85
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2023-09-02 19:19:04 -0300

    Update ClaimNFT.tsx

--- src/components/nft/ClaimNFT.tsx
+++ src/components/nft/ClaimNFT.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Button, Flex, Text } from '@chakra-ui/react';
 import { BigNumber } from 'ethers';
+import { getAddress } from 'ethers/lib/utils';
 import * as React from 'react';
 import robotPfp from '../../assets/img/robot-token-page.svg';
 import { useCurrentEpoch } from '../../chain-interaction/contracts';
@@ -16,7 +17,8 @@ const ClaimNFT = () => {
 
   function generateNFT() {
     if (nftSnapshot && account) {
-      const signature = nftSnapshot.signatures[currentEpoch][account];
+      const signature =
+        nftSnapshot.signatures[currentEpoch][getAddress(account)];
 
       sendClaim(
         {
